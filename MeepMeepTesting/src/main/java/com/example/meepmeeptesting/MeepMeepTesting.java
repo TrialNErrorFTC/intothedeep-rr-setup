1486d9382a0518d632ac9bb05a2ba664a0383deb
Abhinav Karthikeyan <trialnerror42@gmail.com>
2025-02-08 00:52:55 -0500

    im working this at home

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,29 +1,45 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 16)
                 .build();
+        TrajectoryActionBuilder preclipDropPosition = myBot.getDrive().actionBuilder(new Pose2d(14, 62, Math.toRadians(180)))
+                .lineToX(56-8)
+                .endTrajectory();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .build());
+        TrajectoryActionBuilder goToFirstBlock = preclipDropPosition.fresh()
+                .splineToLinearHeading(new Pose2d(new Vector2d(47, 39), Math.toRadians(270)), Math.toRadians(270))
+                .endTrajectory();
+
+        TrajectoryActionBuilder goToRegularDropPosition = goToFirstBlock.fresh()
+                        .setReversed(true)
+                        .splineTo(new Vector2d(56, 57), Math.toRadians(45))
+                .endTrajectory();
+
+        TrajectoryActionBuilder goToSecondBlock = goToRegularDropPosition.fresh()
+                .setReversed(false)
+                .splineTo(new Vector2d(58, 39), Math.toRadians(270))
+                .endTrajectory();
+
+        myBot.runAction(new SequentialAction(
+                preclipDropPosition.build(),
+                goToFirstBlock.build(),
+                goToRegularDropPosition.build(),
+                goToSecondBlock.build()
+        ));
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BucketAuto.java
@@ -0,0 +1,101 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.AccelConstraint;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.AngularVelConstraint;
+import com.acmerobotics.roadrunner.Arclength;
+import com.acmerobotics.roadrunner.InstantAction;
+import com.acmerobotics.roadrunner.MinVelConstraint;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Pose2dDual;
+import com.acmerobotics.roadrunner.PosePath;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.ProfileAccelConstraint;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.VelConstraint;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Autonomous
+public class BClipAuto extends TeleOpActionsRR {
+    FtcDashboard dash = FtcDashboard.getInstance();
+    TelemetryPacket packet = new TelemetryPacket();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d initialPose = new Pose2d(-22+13.5,72-10, Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        ActionControl actionControl = new ActionControl();
+
+        TrajectoryActionBuilder preclipDropPosition = drive.actionBuilder(new Pose2d(14, 62, Math.toRadians(180)))
+                .lineToX(56-8)
+                .endTrajectory();
+
+        TrajectoryActionBuilder goToFirstBlock = preclipDropPosition.fresh()
+                .splineToLinearHeading(new Pose2d(new Vector2d(47, 39), Math.toRadians(270)), Math.toRadians(270))
+                .endTrajectory();
+
+        TrajectoryActionBuilder goToRegularDropPosition = goToFirstBlock.fresh()
+                .setReversed(true)
+                .splineTo(new Vector2d(56, 57), Math.toRadians(45))
+                .endTrajectory();
+
+        TrajectoryActionBuilder goToSecondBlock = goToRegularDropPosition.fresh()
+                .setReversed(false)
+                .splineTo(new Vector2d(58, 39), Math.toRadians(270))
+                .endTrajectory();
+
+
+        Action preclipDropAction = new ParallelAction(
+                preclipDropPosition.build(),
+                actionControl.drop().getAction()
+        );
+        Action goToFirstBlockAction = new ParallelAction(
+                goToFirstBlock.build(),
+                actionControl.setServoAnglePosition(90/300).getAction()
+        );
+        Action regularDropAction = goToRegularDropPosition.build();
+        Action goToSecondBlockAction = goToSecondBlock.build();
+
+        // Init claw Zero extension, then angle
+        Actions.runBlocking(
+                new SequentialAction(
+                        new ParallelAction(
+                                actionControl.setServoAnglePosition(0.0).getAction(),
+                                actionControl.clawClose().getAction(),
+                                actionControl.setSwingPosition(0.0).getAction(),
+                                actionControl.lightOff().getAction()
+                        ),
+                        actionControl.retainAnglePosition().getAction(),
+                        actionControl.zeroExtension().getAction(),
+                        actionControl.zeroAngle().getAction(),
+                        actionControl.zeroExtension().getAction()
+                ));
+
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addLine("Init Position");
+            telemetry.update();
+        }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+
+        );
+    }
+}
